55bfa6bdd94423ebd27b7525b7f0d0e2b92e5164
monsterb <blackzaket@gmail.com>
2021-09-26 17:44:20 +0900

    첫 commit

---
+++ app/src/main/java/com/monsterb/sampleretrofit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.monsterb.sampleretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
